1e95cb1caf0d3e6e882c64993e9d3cf0140e9a4a
Alexander Birrell <146288671+alexb-osucascades@users.noreply.github.com>
2023-10-01 15:41:58 -0700

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# Exercise 0: Code Self-Check
+# Alexander Birrell
+# This program prints out the square roots of the numbers 1 to 1000, inclusive
+from math import sqrt
 
+def printSquareRoots():
+    currentNumber = 1
+    while currentNumber <= 1000:
+        print(sqrt(currentNumber))
+        currentNumber = currentNumber + 1
+
+
+printSquareRoots()
